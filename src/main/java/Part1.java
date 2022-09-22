f6ad726317f347e78b94e4e6524f41149d31c205
Valerie Bayard <valerie.bayard@gmail.com>
2022-09-22 13:33:18 +0100

    added 5 tests for Part1 class

--- src/main/java/Part1.java
+++ src/main/java/Part1.java
@@ -2,17 +2,17 @@ public class Part1 {
     public String findSimpleGene(String dna) {
         int startIndex = dna.indexOf("ATG");
         if (startIndex == -1) {
-            return "This sample contains no DNA stand.";
+            return "No start codon identified: this sample contains no DNA strand.";
         }
         int stopIndex = dna.indexOf("TAA", startIndex + 3);
         if (stopIndex == -1) {
-            return "This sample contains no DNA stand.";
+            return "No stop codon identified: this sample contains no DNA strand.";
         }
         boolean isValidStrand = (dna.substring(startIndex, stopIndex + 3).length()) % 3 == 0;
         if (isValidStrand) {
             return dna.substring(startIndex, stopIndex + 3);
         } else {
-            return "This sample contains no DNA stand.";
+            return "Invalid DNA sequence";
         }
     }
 }
---
+++ src/test/java/Part1Test.java
@@ -0,0 +1,46 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Part1Test {
+
+    private Part1 testGene;
+
+    @Before
+    public void before() {
+        testGene = new Part1();
+
+
+
+
+
+//        System.out.println(testGene.findSimpleGene(noMultipleOfThree));
+    }
+
+    @Test
+    public void canFindValidGene() {
+        String validString = "ATGGGGGTTACCAATTAA";
+        assertEquals("ATGGGGGTTACCAATTAA",testGene.findSimpleGene(validString));
+    }
+
+    @Test
+    public void findsNoGeneIfNoStartCodon() {
+        String noStartCodon = "ATCGGGGTTACCAATAA";
+        String noStartOrStopCodon = "ATCGGGGTTACCAATAC";
+        assertEquals("No start codon identified: this sample contains no DNA strand.",testGene.findSimpleGene(noStartCodon));
+        assertEquals("No start codon identified: this sample contains no DNA strand.",testGene.findSimpleGene(noStartOrStopCodon));
+    }
+
+    @Test
+    public void findsNoGeneIfNoStopCodon() {
+        String noStopCodon = "ATGGGGGTTACCAATAC";
+        assertEquals("No stop codon identified: this sample contains no DNA strand.",testGene.findSimpleGene(noStopCodon));
+    }
+
+    @Test
+    public void findsNoGeneIfStrandNotMultipleOfThree() {
+        String noMultipleOfThree = "ATGGGGGTTACCAATAA";
+        assertEquals("Invalid DNA sequence",testGene.findSimpleGene(noMultipleOfThree));
+    }
+
+}
