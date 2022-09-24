5107a146b138c49caf2a23ce421813c7115b60ee
Valerie Bayard <valerie.bayard@gmail.com>
2022-09-24 15:23:57 +0100

    Created part4 test and part 4 class, tests passing

--- src/main/java/Part4.java
+++ src/main/java/Part4.java
@@ -1,2 +1,16 @@
+import edu.duke.URLResource;
+
 public class Part4 {
+    public void findYTLinks() {
+        URLResource targetWebPage = new URLResource("https://www.dukelearntoprogram.com//course2/data/manylinks.html");
+        for (String word: targetWebPage.words()) {
+            // check if youtube.com is in the link converted to lowercase
+            if (word.toLowerCase().indexOf("youtube.com") > -1) {
+                int startIndex = word.indexOf("\"");
+                int stopIndex = word.lastIndexOf("\"");
+                String toPrint = word.substring(startIndex+1,stopIndex+1);
+                System.out.println(toPrint);
+            }
+        }
+    }
 }
--- src/test/java/Part4Test.java
+++ src/test/java/Part4Test.java
@@ -1,6 +1,9 @@
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import edu.duke.URLResource;
 
@@ -8,15 +11,32 @@ import edu.duke.URLResource;
 public class Part4Test {
     private Part4 YTtest;
     private URLResource myWebPage;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream(); // create a new output stream called outContent
+    private final PrintStream originalOut = System.out; // assign standard output stream to originalOut variable
 
     @Before
     public void before() {
+        System.setOut(new PrintStream(outContent)); // assign outContent to be tbe new output stream
         YTtest = new Part4();
         myWebPage = new URLResource("https://www.dukelearntoprogram.com//course2/data/manylinks.html");
     }
 
     @Test
-    public void canPrintFirstYTLink() {
-        assertEquals("https://www.youtube.com/watch?v=sdzjcBhrgp0",YTtest.findYTLinks());
+    public void canAssertOutputValue() {
+        System.out.println("This line is printed");
+        assertEquals("This line is printed", outContent.toString().trim());
     }
+
+    @Test
+    public void canPrintFirstYTLink() {
+        String result = "http://www.YouTube.com/watch?v=sdzjcBhrgp0";
+        YTtest.findYTLinks();
+        assertEquals(result, outContent.toString().trim().substring(0,result.length()));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut); // reassign originalOut to output stream
+    }
+
 }
